d18e00461d0e2c87f617753aceb17dd026909553
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2021-10-28 00:26:17 +0200

    lab3

--- lab/lab3.py
+++ lab/lab3.py
@@ -73,4 +73,45 @@ def es0():
     plt.show()
 
 def es1():
-    
+    pass # TODO
+
+def es2():
+    A = plt.imread('Stereo-1_channel_image_of_Phobos_ESA214117.jpg')
+    A = A[3000:4000, 5000:6000]
+    print(A.shape)
+    plt.imshow(A, cmap='gray')
+    plt.show()    
+    U, s, Vh = scipy.linalg.svd(A)
+    print('Shape of U:', U.shape)
+    print('Shape of s:', s.shape)
+    print('Shape of V:', Vh.T.shape)
+    A_p = np.zeros(A.shape)
+    p_max = 20
+    err_rel = np.zeros((p_max))
+    c = np.zeros((p_max))
+
+    for i in range(p_max):
+        ui = U[ : , i]
+        vi = Vh[i, : ]
+        A_p = A_p + np.outer(ui, vi) * s[i]
+        err_rel[i] = np.linalg.norm(A_p - A) / np.linalg.norm(A)
+        c[i] = min(A.shape) / (i + 1) - 1
+
+    print('\nL\'errore relativo della ricostruzione di A è', err_rel[-1])
+    print('Il fattore di compressione è c = ', c[-1])
+    plt.figure(figsize=(10, 5))
+    fig1 = plt.subplot(1, 2, 1)
+    fig1.plot(err_rel, 'o-')
+    plt.title('Errore relativo')
+    fig2 = plt.subplot(1, 2, 2)
+    fig2.plot(c, 'o-')
+    plt.title('Fattore di compressione')
+    plt.show()
+    plt.figure(figsize=(20, 10))
+    fig1 = plt.subplot(1, 2, 1)
+    fig1.imshow(A, cmap='gray')
+    plt.title('True image')
+    fig2 = plt.subplot(1, 2, 2)
+    fig2.imshow(A_p, cmap='gray')
+    plt.title('Reconstructed image with p =' + str(p_max))
+    plt.show()
