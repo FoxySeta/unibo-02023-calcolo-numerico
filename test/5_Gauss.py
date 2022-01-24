04d86b1e81925f11a10e16027586dc81fdafcf12
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2022-01-24 22:58:56 +0100

    Yet another Gauss exercise

--- test/5_Gauss.py
+++ test/5_Gauss.py
@@ -2,8 +2,8 @@ import numpy as np
 from scipy.linalg import hilbert
 import scipy.linalg.decomp_lu as dl
 
-n = 7
-A = hilbert(n)
+n = 6
+A = 4 * np.eye(n) + np.diag(np.ones(n - 1), 1) + np.diag(np.ones(n - 1), -1)
 print("A =\n", A)
 print("K = ", np.linalg.cond(A, 2))
 x = np.ones(n)
